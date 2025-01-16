b0ccaeb67223ba55cb3108b42a3693caa1ba310d
Sorted Pixel <sorted@Sorteds-MacBook-Air-4.local>
2025-01-16 12:50:37 +0530

    test  setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. first app
+          Edit <code>src/App.js</code> and save to reload. first app 12345
         </p>
 
         <a
